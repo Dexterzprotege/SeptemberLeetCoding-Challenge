277f5463c7ff675f64fa2e3b3ce1f8d0c4589072
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2020-09-06 09:27:27 +0530

    Update 01LargestTimeforGivenDigits.py

--- 01LargestTimeforGivenDigits.py
+++ 01LargestTimeforGivenDigits.py
@@ -1,4 +1,4 @@
-```Given an array of 4 digits, return the largest 24 hour time that can be made.
+"""Given an array of 4 digits, return the largest 24 hour time that can be made.
 The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:00, a time is larger if more time has elapsed since midnight.
 Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
 
@@ -20,7 +20,7 @@ Output: "10:00"
  
 Constraints:
 arr.length == 4
-0 <= arr[i] <= 9```
+0 <= arr[i] <= 9"""
 
 class Solution:
     def largestTimeFromDigits(self, arr: List[int]) -> str:
