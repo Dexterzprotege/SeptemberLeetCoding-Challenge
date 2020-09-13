ebc926bc870262b367ab05d1e4ef1cf446ebbc9b
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2020-09-13 20:11:00 +0530

    Create 13InsertInterval.py

---
+++ 13InsertInterval.py
@@ -0,0 +1,31 @@
+'''Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.'''
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        for interval in intervals:
+            # Non over-lapping case
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            # Overlapping case
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            # Merge case
+            elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+        result.append(newInterval)
+        return result
+        
