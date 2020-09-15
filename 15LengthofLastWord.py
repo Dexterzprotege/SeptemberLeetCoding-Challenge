8b13a74fc487753d362194b4916ccf6ac7f7d08a
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2020-09-15 18:03:06 +0530

    Create 15LengthofLastWord.py

---
+++ 15LengthofLastWord.py
@@ -0,0 +1,14 @@
+'''Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+If the last word does not exist, return 0.
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+Example:
+Input: "Hello World"
+Output: 5'''
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        arr = s.split()
+        if len(arr)==0:
+            return 0
+        return len(arr[-1])
