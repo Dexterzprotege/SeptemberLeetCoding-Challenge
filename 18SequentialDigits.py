5a1b4d70f9406014094aca4bd2ab49d37db2eadc
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2020-09-20 08:56:35 +0530

    Create 18SequentialDigits.py

---
+++ 18SequentialDigits.py
@@ -0,0 +1,27 @@
+'''An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+Example 1:
+Input: low = 100, high = 300
+Output: [123,234]
+
+Example 2:
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+ 
+Constraints:
+10 <= low <= high <= 10^9'''
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        ans = []
+        for i in range(1, 10):
+            number = i
+            nextNum = i
+            while(number<=high and nextNum <10):
+                if number>=low:
+                    ans.append(number)
+                nextNum += 1
+                number = number*10+nextNum
+                
+        return sorted(ans)
+                
